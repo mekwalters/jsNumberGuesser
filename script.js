2ffd3e00cb257cda99812748e4328e0f7ed1b34c
mekwalters <mekwalters@gmail.com>
2023-06-27 15:48:48 -0400

    works with change in advanceRoud function

--- script.js
+++ script.js
@@ -4,26 +4,27 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 const generateTarget =() => {
-  return Math.floor(Math.random()) * 10;
+  return Math.floor(Math.random()*10);
 }
 
 function compareGuesses (humanGuess, computerGuess, targetGuess) {
-  const humanDifference = Math.abs(humanGuess-targetGuess);
-  const computerDifference = Math.abs(computerGuess-targetGuess);
+  const humanDifference = Math.abs(targetGuess-humanGuess);
+  const computerDifference = Math.abs(targetGuess -computerGuess);
   if (humanDifference <= computerDifference) {
     return true;
   } else {
     return false;
   }
 } 
+// let winner = compareGuesses (humanGuess, computerGuess, targetGuess); //shouldn't need because game.js assigns in line 30
 
 function updateScore(winner) {
   if (winner ==='human') {
     humanScore++;
   } else if (winner === 'computer') {
-    computerScore = computerScore++;
+    computerScore++;
   }
 } 
-function advanceRound(currentRoundNumber) {
+
+const advanceRound = () => 
   currentRoundNumber++;
-}
